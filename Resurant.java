5dccf0faa833a81e47fd082f84c360eca46ecd3d
Hannanmirzabaig <112794557+Hannanmirzabaig@users.noreply.github.com>
2023-10-03 00:50:54 +0530

    Update Resturant.java

--- Resurant.java
+++ Resurant.java
@@ -1,6 +1,6 @@
 package Night_session_Test;
 import java.util.Scanner;
-public class Resurant {
+public class Resturant {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		
